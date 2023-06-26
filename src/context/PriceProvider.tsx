b1f6e97df23caae1d3b8574c840d67911c8a9cd5
Yongveloper <aaa9015@naver.com>
2023-06-27 02:36:29 +0900

    [기능] 천자리 콤마 필터링

--- src/context/PriceProvider.tsx
+++ src/context/PriceProvider.tsx
@@ -7,18 +7,18 @@ import {
   ReactNode,
 } from 'react';
 
-export type PriceType = string | null;
+export type PriceType = number | null;
 
-const defaultPrice: PriceType = '';
+const defaultPrice: PriceType = 0;
 
 interface IPriceContext {
   price: PriceType;
-  setPrice: Dispatch<SetStateAction<PriceType>>;
+  setPrice: (value: string | null) => void;
 }
 
 const PriceContext = createContext<IPriceContext>({
   price: defaultPrice,
-  setPrice: () => {},
+  setPrice: (value: string | null) => {},
 });
 
 export const usePriceContext = () => {
@@ -32,8 +32,16 @@ interface PriceProviderProps {
 export const PriceProvider: React.FC<PriceProviderProps> = ({ children }) => {
   const [price, setPrice] = useState<PriceType>(defaultPrice);
 
+  const updatePrice = (value: string | null) => {
+    if (value !== null) {
+      const numberWithCommasRemoved = value.replace(/,/g, '');
+      const number = parseFloat(numberWithCommasRemoved);
+      setPrice(number);
+    }
+  };
+
   return (
-    <PriceContext.Provider value={{ price, setPrice }}>
+    <PriceContext.Provider value={{ price, setPrice: updatePrice }}>
       {children}
     </PriceContext.Provider>
   );
