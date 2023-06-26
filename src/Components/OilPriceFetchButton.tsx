bf49e014500b998d5d3a6f21ac6390f83bc61034
Yongveloper <aaa9015@naver.com>
2023-06-27 02:05:48 +0900

    [스타일] 가격 불러오기 버튼 스타일 수정 로딩 모션 추가

--- src/Components/OilPriceFetchButton.tsx
+++ src/Components/OilPriceFetchButton.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
 import { Box, Button } from '@mui/material';
 import CachedIcon from '@mui/icons-material/Cached';
+import { LoadingButton } from '@mui/lab';
 
 interface IOilPriceFetchButtonProps {
+  loading: boolean;
   onClick: () => void;
 }
 
-function OilPriceFetchButton({ onClick }: IOilPriceFetchButtonProps) {
+function OilPriceFetchButton({ loading, onClick }: IOilPriceFetchButtonProps) {
   return (
-    <Box
-      sx={{
-        marginBottom: '0.35em',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
+    <LoadingButton
+      size="medium"
+      onClick={onClick}
+      loading={loading}
+      loadingIndicator="불러오는중..."
+      variant="outlined"
     >
-      <Button
-        variant="contained"
-        size="small"
-        startIcon={<CachedIcon />}
-        onClick={onClick}
-      >
-        전국 주유소 평균 가격 조회하기
-      </Button>
-    </Box>
+      <CachedIcon sx={{ marginRight: '4px' }} />
+      <span>전국 주유소 평균 가격 조회하기</span>
+    </LoadingButton>
   );
 }
 
