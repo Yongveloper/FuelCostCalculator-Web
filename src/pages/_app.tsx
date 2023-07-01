d6da553de9a21e2148bd45a77066e466a61f95e4
Yongveloper <aaa9015@naver.com>
2023-07-01 13:33:43 +0900

    SEO최적화

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,38 @@ import { PriceProvider } from '@/context/PriceProvider';
 import '@/styles/fonts.css';
 import '@/styles/global.css';
 import Layout from '@/Components/Layout';
+import { DefaultSeo } from 'next-seo';
+
+const DEFAULT_SEO = {
+  title: '기름값 계산기 - 쉽고 빠른 주유비 계산',
+  description:
+    '쉽고 빠르게 주유비, 기름값, 유류비 계산을 해보세요!, 기름값,유류비,주유비 계산기는 여러분의 자동차의 연료 소비와 비용을 추적하고 분석하여 효율적인 연료 비용 관리를 돕는 도구입니다.',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://fuelcostcalculator.vercel.app/',
+    title: '기름값 계산기 - 쉽고 빠른 주유비 계산',
+    site_name: '기름값 계산기 - 쉽고 빠른 주유비 계산',
+    images: [
+      {
+        url: 'https://fuelcostcalculator.vercel.app/logo.png',
+        width: 285,
+        height: 167,
+        alt: '대표 이미지',
+      },
+    ],
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PriceProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </PriceProvider>
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <PriceProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PriceProvider>
+    </>
   );
 }
--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Divider, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import { ResultPageQuery } from '@/types';
+import { NextSeo } from 'next-seo';
 
 function Result() {
   const router = useRouter();
@@ -15,108 +16,111 @@ function Result() {
   };
 
   return (
-    <Box sx={{ minWidth: '320px' }}>
-      <Box
-        sx={{
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'space-between',
-          marginBottom: '4px',
-        }}
-      >
-        <Typography variant="h6" component="span">
-          운행거리:
-        </Typography>
-        <Typography variant="h6" component="span">
-          {numberWithCommas(distance) || 'null'}(km)
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'space-between',
-          marginBottom: '4px',
-        }}
-      >
-        <Typography variant="h6" component="span">
-          평균 연비:
-        </Typography>
-        <Typography variant="h6" component="span">
-          {numberWithCommas(fuelEfficiency) || 'null'}(km/L)
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Typography variant="h6" component="span">
-          유류 가격:
-        </Typography>
-        <Typography variant="h6" component="span">
-          {numberWithCommas(price) || 'null'}(원/L)
-        </Typography>
-      </Box>
+    <>
+      <NextSeo canonical="https://fuelcostcalculator.vercel.app/result" />
+      <Box sx={{ minWidth: '320px' }}>
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: '4px',
+          }}
+        >
+          <Typography variant="h6" component="span">
+            운행거리:
+          </Typography>
+          <Typography variant="h6" component="span">
+            {numberWithCommas(distance) || 'null'}(km)
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: '4px',
+          }}
+        >
+          <Typography variant="h6" component="span">
+            평균 연비:
+          </Typography>
+          <Typography variant="h6" component="span">
+            {numberWithCommas(fuelEfficiency) || 'null'}(km/L)
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Typography variant="h6" component="span">
+            유류 가격:
+          </Typography>
+          <Typography variant="h6" component="span">
+            {numberWithCommas(price) || 'null'}(원/L)
+          </Typography>
+        </Box>
 
-      <Box sx={{ width: '100%' }}>
-        <Divider sx={{ marginY: '12px' }} />
-        <Box
-          sx={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginBottom: '8px',
-          }}
-        >
-          <Typography variant="h5" component="span">
-            예상 주유량:
-          </Typography>
-          <Typography variant="h5" component="span">
-            <Typography
-              variant="h5"
-              component="span"
-              sx={{ fontWeight: 'bold' }}
-            >
-              {expectedFuelVolume || 'null'}
+        <Box sx={{ width: '100%' }}>
+          <Divider sx={{ marginY: '12px' }} />
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginBottom: '8px',
+            }}
+          >
+            <Typography variant="h5" component="span">
+              예상 주유량:
             </Typography>
-            (L)
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Typography variant="h5" component="span">
-            예상 주유비:
-          </Typography>
-          <Typography variant="h5" component="span">
-            약{' '}
-            <Typography
-              variant="h5"
-              component="span"
-              sx={{ fontWeight: 'bold' }}
-            >
-              {numberWithCommas(expectedPrice) || 'null'}
+            <Typography variant="h5" component="span">
+              <Typography
+                variant="h5"
+                component="span"
+                sx={{ fontWeight: 'bold' }}
+              >
+                {expectedFuelVolume || 'null'}
+              </Typography>
+              (L)
             </Typography>
-            (원)
-          </Typography>
+          </Box>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography variant="h5" component="span">
+              예상 주유비:
+            </Typography>
+            <Typography variant="h5" component="span">
+              약{' '}
+              <Typography
+                variant="h5"
+                component="span"
+                sx={{ fontWeight: 'bold' }}
+              >
+                {numberWithCommas(expectedPrice) || 'null'}
+              </Typography>
+              (원)
+            </Typography>
+          </Box>
         </Box>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ width: '100%', maxWidth: '320px', marginTop: '26px' }}
+          onClick={handleButtonClick}
+        >
+          다시 계산하기
+        </Button>
       </Box>
-      <Button
-        variant="contained"
-        size="large"
-        sx={{ width: '100%', maxWidth: '320px', marginTop: '26px' }}
-        onClick={handleButtonClick}
-      >
-        다시 계산하기
-      </Button>
-    </Box>
+    </>
   );
 }
 
