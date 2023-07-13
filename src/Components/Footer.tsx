e8df3081e92473bd4c87af3f77875b96e05ee100
Yongveloper <aaa9015@naver.com>
2023-07-13 21:46:51 +0900

    [스타일] 카카오 광고 크기 설정 큰 화면에서 너무 커지는 문제 해결

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,18 +6,20 @@ function Footer() {
       sx={{ width: '100%', mt: 2, textAlign: 'center', color: 'gray' }}
       component="footer"
     >
-      <ins
-        className="kakao_ad_area"
-        style={{ display: 'none' }}
-        data-ad-unit="DAN-tzLsnHJx4Gf6MSfP"
-        data-ad-width="320"
-        data-ad-height="100"
-      ></ins>
-      <script
-        type="text/javascript"
-        src="//t1.daumcdn.net/kas/static/ba.min.js"
-        async
-      ></script>
+      <Box sx={{ maxWidth: '320px', margin: 'auto' }}>
+        <ins
+          className="kakao_ad_area"
+          style={{ display: 'none' }}
+          data-ad-unit="DAN-tzLsnHJx4Gf6MSfP"
+          data-ad-width="320"
+          data-ad-height="100"
+        ></ins>
+        <script
+          type="text/javascript"
+          src="//t1.daumcdn.net/kas/static/ba.min.js"
+          async
+        ></script>
+      </Box>
       <Typography variant="body2" component="span">
         Contact: yongyong5766@gmail.com
         <br />© yongyong Co. all rights reserved.
